34d2e287d13904ac3f786f7c37d4e2f60fb83af8
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-16 12:16:30 +0700

    Create MySQLPingTask.php

---
+++ src/NurAzliYT/CoinAPI/task/MySQLPingTask.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace NurAzliYT\CoinAPI\task;
+
+
+use NurAzliYT\CoinAPI\CoinAPI;
+use pocketmine\scheduler\Task;
+
+class MySQLPingTask extends Task{
+    private $mysql;
+    
+    private $plugin;
+
+    public function __construct(CoinAPI $plugin, \mysqli $mysql){
+        $this->plugin = $plugin;
+
+        $this->mysql = $mysql;
+    }
+
+    public function onRun(): void{
+        if(!$this->mysql->ping()){
+            $this->plugin->openProvider();
+        }
+    }
+}
