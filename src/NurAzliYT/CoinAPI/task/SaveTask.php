4b112238764ba15e7c21f67e2a75bb2ca16726c7
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-16 12:05:47 +0700

    Create SaveTask.php

---
+++ src/NurAzliYT/CoinAPI/task/SaveTask.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace NurAzliYT\CoinAPI\task;
+
+use NurAzliYT\CoinAPI\CoinAPI;
+
+use pocketmine\scheduler\Task;
+
+class SaveTask extends Task {
+    private $plugin;
+    public function __construct(CoinAPI $plugin){
+        $this->plugin = $plugin;
+    }
+
+    public function onRun() : void{
+        $this->plugin->saveAll();
+    }
+}
