3209821166a3aed4e599b933e0e5b17a0cc7e90d
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-27 14:37:19 +0700

    Add files via upload

--- src/NurAzliYT/CoinAPI/event/coin/PayCoinEvent.php
+++ src/NurAzliYT/CoinAPI/event/coin/PayCoinEvent.php
@@ -1,9 +1,27 @@
 <?php
 
-namespace NurAzliYT\CoinAPI\event\coin;
+/*
+ * PointS, the massive point plugin with many features for PocketMine-MP
+ * Copyright (C) 2013-2017  onebone <jyc00410@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-use NurAzliYT\CoinAPI\event\CoinAPIEvent;
-use NurAzliYT\CoinAPI\CoinAPI;
+namespace onebone\coinapi\event\coin;
+
+use onebone\coinapi\event\CoinAPIEvent;
+use onebone\coinapi\CoinAPI;
 
 class PayCoinEvent extends CoinAPIEvent{
 	private $payer, $target, $amount;
