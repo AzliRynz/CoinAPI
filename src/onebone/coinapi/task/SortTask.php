078e82cfb364aacf9bef25178fdf9f0b34ee917e
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-07 15:17:07 +0700

    Update SortTask.php

--- src/onebone/coinapi/task/SortTask.php
+++ src/onebone/coinapi/task/SortTask.php
@@ -1,30 +1,11 @@
 <?php
-
-/*
- * PointS, the massive point plugin with many features for PocketMine-MP
- * Copyright (C) 2013-2017  onebone <jyc00410@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-namespace onebone\coinapi\task;
+namespace NurAzliYT\CoinAPI\task;
 
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 use pocketmine\player\Player;
 
-use onebone\coinapi\CoinAPI;
+use NurAzliYT\CoinAPI\CoinAPI;
 
 class SortTask extends AsyncTask{
     private $sender, $rebirthcoinData, $addOp, $page, $ops, $banList;
